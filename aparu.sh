b517b7d030a4f1e15a71d1b7d0aa046fac70af5a
JumperLuko <jumper.luko@gmail.com>
2022-03-16 15:32:35 -0300

    Autocomplete works + --yes + remove + update + upgrade

--- aparu.sh
+++ aparu.sh
@@ -4,5 +4,25 @@ case $1 in
 	upgrade)
 		paru ;;
 	install)
-		paru -S ${@:2} ;;
+		case $2 in
+			"--yes")
+				paru -S --noconfirm "${@:3}" ;;
+			*)
+				paru -S "${@:2}" ;;
+		esac ;;
+	remove)
+		paru -R "${@:2}" ;;
+	update)
+		paru -Sy ;;
+	upgrade)
+		case $2 in
+			"--yes")
+				paru -Syu --noconfirm ;;
+			*)
+				paru -Syu ;;
+		esac ;;
+	*)
+		paru "${@:2}" ;;
 esac
+
+#? how to put --yes in any position? Simple way, please
